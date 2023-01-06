ea454b45c123f4de1e29c93d92a5670cd9c02763
Kathanshi Jain <121633809+Kathanshi0402@users.noreply.github.com>
2023-01-06 12:51:55 +0530

    Update Factorian.py

--- Factorian.py
+++ Factorian.py
@@ -1,5 +1,5 @@
 j=0
-while j<=40585:
+while j<=40585:    #40585 is the largest factorian
     n=j
     temp=n
     f=0
@@ -7,11 +7,9 @@ while j<=40585:
         a=n%10
         fact=1
         for i in range(1,a+1):
-            fact=fact*i
+            fact=fact*i       #To find factorial of "a"
         f=f+fact
         n=n//10
     if temp==f:
         print(temp)
     j=j+1
-
-
